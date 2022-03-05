f818187ffc6e52e46c5ae440243995b84521bcca
safaakedidi <safakedidi30@gmail.com>
2022-03-06 00:57:44 +0100

    first nextjs

--- pages/404.js
+++ pages/404.js
@@ -3,21 +3,25 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 
 const NotFound = () => {
-const router=useRouter();
-    useEffect(() => {
-        
-      setTimeout(()=>{
-          router.push("/");
-      },3000);
-    }, [])
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);
 
-    return ( 
-        <div className="not-found">
-            <h1>ooooops...</h1>
-            <p>that page cannot be found</p>
-            <p>Go back to <Link href="/"><a>Homepage</a></Link></p>
-        </div>
-     );
-}
- 
+  return (
+    <div className="not-found">
+      <h1>ooooops...</h1>
+      <p>that page cannot be found</p>
+      <p>
+        Go back to{' '}
+        <Link href="/">
+          <a>Homepage</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
 export default NotFound;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,38 @@
 import Head from 'next/dist/shared/lib/head';
 
 import Link from 'next/link';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <>
       <div>
         <Head>
           <title>First List | Home</title>
-          <meta name="keywords" content="ninjas" />
+          <meta name="keywords" content="users" />
         </Head>
         <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-        <Link href="/ninjas" ><a className={styles.btn}>see ninjas list</a></Link>
-
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <Link href="/users">
+          <a className={styles.btn}>see users list</a>
+        </Link>
       </div>
     </>
-  )
+  );
 }
